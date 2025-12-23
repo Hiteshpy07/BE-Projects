2537668856f843bdcdd39873deeb6271b4c4dfa9
Hiteshpy07 <kumarnayakhitesh@gmail.com>
2025-12-23 15:45:34 +0530

    db connected

--- blogsite/server.js
+++ blogsite/server.js
@@ -1,5 +1,17 @@
 const express=require('express');
 const app=express();
+const mongoose = require('mongoose');
+const Post = require('./models/Post');
+// require('dotenv').config();
+
+
+app.use(express.json());
+
+
+mongoose.connect("mongodb://localhost:27017/blogsite")
+  .then(() => console.log("DB Connected"))
+  .catch(err => console.error(err));
+  
 
 app.get("/",(req,res)=>{
     res.send("Welcome to the Blog Site!");
@@ -8,8 +20,19 @@ app.get("/blogs",(req,res)=>{
     res.send("Here are all the blog posts.");
 })
 
-app.post("/post-blog",(req,res)=>{
-    res.send("New blog post created!");
+app.post("/post-blog",async(req,res)=>{
+    const title=req.body.title;
+    const content=req.body.content;
+    
+
+    const newPost = new Post({
+        title: title,
+        content: content})
+
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+    // res.send("New blog post created!");
+    // console.log(`New blog post titled "${title}" created with content: ${content}`);
     console.log("A new blog post has been created.");
 })
 
